9df43e8f8d66bafba136c53ffcdf6aa7ad7ea833
TurtRabi <minhoan305@gmail.com>
2024-09-24 15:59:42 +0700

    update: collision between object

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -23,11 +23,13 @@ public class GamePanel extends JPanel implements Runnable {
 
     public  final  int maxWorldCol=50;
     public  final  int maxWorldRow=50;
+
     public final int worldWidth = maxWorldCol*tileSize;
     public final int worldHeight = maxWorldRow*tileSize;
 
     Thread gameThread;
 
+    public CollisionChecker collisionChecker = new CollisionChecker(this);
     public Player player = new Player(this,keyHandler);
     TileManager tileManager = new TileManager(this);
 
